ea9c6214eb6f019ede1e1ffcc91d2dd03ec96cd5
MALWICK <batahumphrey66@gamil.com>
2024-01-08 16:56:08 +0100

    updating routes paths

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -1,1 +1,23 @@
 import { createRouter, createMemoryHistory } from "vue-router";
+import invoiceIndex from '../component/invoice/index.vue'
+import notFound from "../component/NotFound.vue"
+
+
+const routes = [
+    {
+        path:'/',
+        component: invoiceIndex,
+    },
+    {
+        path:'/:pathMatch(.*)*',
+        component: notFound
+    }
+
+]
+
+const router = createRouter({
+    history: createMemoryHistory(),
+    routes
+})
+
+export default router;
